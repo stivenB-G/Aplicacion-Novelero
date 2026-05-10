27c1c0ec1bcaaeda0942973ff8ec11ff082ce338
Jesus Castiblanco <jedacaga@gmail.com>
2026-05-10 09:30:03 -0500

    "Agregados fragmentos de Perfil, Pago y Autor"

--- app/src/main/java/com/example/appnovelero/ui/author/AuthorProfileFragment.kt
+++ app/src/main/java/com/example/appnovelero/ui/author/AuthorProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.example.appnovelero.ui.profile
+package com.example.appnovelero.ui.author
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.appnovelero.R
 
-class ProfileFragment : Fragment() {
+class AuthorProfileFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_author_profile, container, false)
     }
 }
--- app/src/main/java/com/example/appnovelero/ui/payment/PaymentFragment.kt
+++ app/src/main/java/com/example/appnovelero/ui/payment/PaymentFragment.kt
@@ -1,4 +1,4 @@
-package com.example.appnovelero.ui.profile
+package com.example.appnovelero.ui.payment
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.appnovelero.R
 
-class ProfileFragment : Fragment() {
+class PaymentFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_payment, container, false)
     }
 }
